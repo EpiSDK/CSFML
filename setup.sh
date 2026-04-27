fba8b85d86fef49e764fb9dbcfeb3c69461fdb09
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-27 18:05:56 +0200

    feat: add setup script to install

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+nix build .#sfml .#csfml
+nix profile add .#sfml .#csfml
+echo '# SET env in your nix conf'
+echo 'export LIBRARY_PATH=$HOME/.nix-profile/lib:$LIBRARY_PATH
+export C_INCLUDE_PATH=$HOME/.nix-profile/include:$C_INCLUDE_PATH
+export CPATH=$HOME/.nix-profile/include:$CPATH
+export PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH
+'
+
