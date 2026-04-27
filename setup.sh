85558446b0307b086a0bfecf84fd08f15b03adc0
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-27 18:13:16 +0200

    feat: add color warning

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 nix build .#sfml .#csfml
 nix profile add .#sfml .#csfml
-echo '# SET env in your nix conf'
-echo 'export LIBRARY_PATH=$HOME/.nix-profile/lib:$LIBRARY_PATH
+echo -e "\033[31m# SET env in your nix conf\033[0m"
+echo -e "\033[34mexport LIBRARY_PATH=$HOME/.nix-profile/lib:$LIBRARY_PATH
 export C_INCLUDE_PATH=$HOME/.nix-profile/include:$C_INCLUDE_PATH
 export CPATH=$HOME/.nix-profile/include:$CPATH
 export PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH'
+export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH\033[0m"
 
 export LIBRARY_PATH=$HOME/.nix-profile/lib:$LIBRARY_PATH
 export C_INCLUDE_PATH=$HOME/.nix-profile/include:$C_INCLUDE_PATH
