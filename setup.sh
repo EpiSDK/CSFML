fe27f86309a45541541903be6260c05bfe8a3bfa
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-27 18:06:35 +0200

    feat: implement env var export

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ echo 'export LIBRARY_PATH=$HOME/.nix-profile/lib:$LIBRARY_PATH
 export C_INCLUDE_PATH=$HOME/.nix-profile/include:$C_INCLUDE_PATH
 export CPATH=$HOME/.nix-profile/include:$CPATH
 export PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH
-'
+export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH'
 
+export LIBRARY_PATH=$HOME/.nix-profile/lib:$LIBRARY_PATH
+export C_INCLUDE_PATH=$HOME/.nix-profile/include:$C_INCLUDE_PATH
+export CPATH=$HOME/.nix-profile/include:$CPATH
+export PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$HOME/.nix-profile/lib:$LD_LIBRARY_PATH
